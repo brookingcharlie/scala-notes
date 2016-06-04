6d2504289d9a2ef5d0e34d6c76cd5ea6cea4a6e5
Charlie Brooking <cbrookin@thoughtworks.com>
2016-06-04 23:02:29 +1000

    use meaningful function names

--- option/option.scala
+++ option/option.scala
@@ -13,18 +13,18 @@ assert(mean(List(1, 2, 3)) == Some(2))
 // returning the result of the function wrapped in a new Some.
 // If the input is None, map returns None.
 
-val f: Int => Int = _ * 2
-assert(Some(1).map(f) == Some(2))
-assert(None.map(f) == None)
+val multiplyBy2: Int => Int = _ * 2
+assert(Some(1).map(multiplyBy2) == Some(2))
+assert(None.map(multiplyBy2) == None)
 
 // flatMap applies a function to the value wrapped in Some,
 // where the function itself can return either Some or None.
 // If the input in None, flatMap returns None.
 
-val g: Int => Option[Int] = x => if (x > 0) Some(x * 2) else None
-assert(Some(1).flatMap(g) == Some(2))
-assert(Some(-1).flatMap(g) == None)
-assert(None.flatMap(g) == None)
+val multiplyPositiveBy2: Int => Option[Int] = x => if (x > 0) Some(x * 2) else None
+assert(Some(1).flatMap(multiplyPositiveBy2) == Some(2))
+assert(Some(-1).flatMap(multiplyPositiveBy2) == None)
+assert(None.flatMap(multiplyPositiveBy2) == None)
 
 // getOrElse extracts the value from Some
 // or returns the given default value for None.
@@ -43,10 +43,10 @@ assert(None.orElse(Some(0)) == Some(0))
 // or None is the predicate returns false.
 // If the input is None, map returns None.
 
-val h: Int => Boolean = x => x > 0
-assert(Some(1).filter(h) == Some(1))
-assert(Some(-1).filter(h) == None)
-assert(None.filter(h) == None)
+val isPositive: Int => Boolean = x => x > 0
+assert(Some(1).filter(isPositive) == Some(1))
+assert(Some(-1).filter(isPositive) == None)
+assert(None.filter(isPositive) == None)
 
 // Implement our own versions of Option functions
 
@@ -54,8 +54,8 @@ def map[X, Y](xs: Option[X])(f: X => Y): Option[Y] = xs match {
   case Some(x) => Some(f(x))
   case None => None
 }
-assert(map(Some(1))(f) == Some(2))
-assert(map(None)(f) == None)
+assert(map(Some(1))(multiplyBy2) == Some(2))
+assert(map(None)(multiplyBy2) == None)
 
 def getOrElse[X, Z >: X](xs: Option[X])(z: => Z): Z = xs match {
   case Some(x) => x
@@ -66,9 +66,9 @@ assert(getOrElse(None)(0) == 0)
 
 def flatMap[X, Y](xs: Option[X])(f: X => Option[Y]): Option[Y] =
   getOrElse(map(xs)(f))(None)
-assert(flatMap(Some(1))(g) == Some(2))
-assert(flatMap(Some(-1))(g) == None)
-assert(flatMap(None)(g) == None)
+assert(flatMap(Some(1))(multiplyPositiveBy2) == Some(2))
+assert(flatMap(Some(-1))(multiplyPositiveBy2) == None)
+assert(flatMap(None)(multiplyPositiveBy2) == None)
 
 def orElse[X, Z >: X](xs: Option[X])(zs: => Option[Z]): Option[Z] =
   getOrElse(map(xs)(x => Some(x): Option[Z]))(zs)
@@ -77,8 +77,8 @@ assert(orElse(None)(Some(0)) == Some(0))
 
 def filter[X](xs: Option[X])(p: X => Boolean): Option[X] =
   flatMap(xs)(x => if (p(x)) Some(x) else None)
-assert(filter(Some(1))(h) == Some(1))
-assert(filter(Some(-1))(h) == None)
-assert(filter(None)(h) == None)
+assert(filter(Some(1))(isPositive) == Some(1))
+assert(filter(Some(-1))(isPositive) == None)
+assert(filter(None)(isPositive) == None)
 
 println("All tests passed!")
